055c5df1829c3592aae9447f771c0c9c5810a8c5
sougata <sougataghar47@gmail.com>
2025-12-23 19:33:11 +0530

    conditional rendering of logout button

--- container.php
+++ container.php
@@ -1,12 +1,15 @@
 <?php
-$html;
-echo '<div class="mx-auto max-w-200 p-4 min-h-screen grid place-items-center">
-<nav class="flex bg-black text-white justify-between w-full items-center rounded-lg">
-<span>User Feedback</span>
-<form method="POST" action="/logout.php">
+$isAdminLoggedIn=isset($_SESSION['admin_id'])?'<form method="POST" action="/logout.php">
 <button type="submit" class="bg-black text-white px-4 py-2 rounded-xl">Logout</button>
-</form>
+</form>':null;
+$html;
+echo '
+<nav class="mx-auto max-w-200 p-4 flex   justify-between w-full items-center rounded-lg">
+<span class=""text-3xl>User Feedback</span>'.$isAdminLoggedIn.'
+
 </nav>
+<div class="mx-auto max-w-200 p-4 min-h-screen grid place-items-center">
+
 '
 .$html.
 '</div>'
