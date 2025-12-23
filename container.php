6fb27510f914ecd346fe0065c534e2f9800be8ee
sougata <sougataghar47@gmail.com>
2025-12-23 18:17:14 +0530

    url changed for session destroy

--- container.php
+++ container.php
@@ -3,7 +3,7 @@ $html;
 echo '<div class="mx-auto max-w-200 p-4 min-h-screen grid place-items-center">
 <nav class="flex bg-black text-white justify-between w-full items-center rounded-lg">
 <span>User Feedback</span>
-<form method="POST" action="./logout.php">
+<form method="POST" action="/logout.php">
 <button type="submit" class="bg-black text-white px-4 py-2 rounded-xl">Logout</button>
 </form>
 </nav>
